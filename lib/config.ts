ec0feb4bb1d0920b15f6d6e4a43c609c993ca76e
Kvazar-213452 <kvazar382@gmail.com>
2026-02-22 00:44:53 +0200

    imp/domain

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
-export const MAIN_DOMEN: string = "http://localhost:3000/";
-export const MAIN_DOMEN_NO: string = "http://localhost:3000";
+export const MAIN_DOMEN: string = "https://www.converterdevtools.com/";
+export const MAIN_DOMEN_NO: string = "https://www.converterdevtools.com";
 export const NAME_MAKE: string = "DevTools Converter";
